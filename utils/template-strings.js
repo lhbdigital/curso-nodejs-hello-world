90a43ad7536a84e9d8135b277a7ae18b3ea43242
H <leonardo@lhbdigital.com.br>
2021-06-04 00:08:28 -0300

    template strings example

---
+++ utils/template-strings.js
@@ -0,0 +1,9 @@
+const myName = "Leonardo";
+
+console.log("Hello, world! My name is " + myName);
+
+console.log(`Hello, world! My name is ${myName} and 2 + 2 = ${2 + 2}`);
+
+console.log(`Multi
+line
+string`);
